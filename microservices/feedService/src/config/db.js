16f5d05ad322d2436cb53a6a821e6cce4560dffa
BasmalaAmr1 <148448444+BasmalaAmr1@users.noreply.github.com>
2025-04-18 16:27:59 +0200

    Update db.js

--- microservices/feedService/src/config/db.js
+++ microservices/feedService/src/config/db.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database Connected!');
+  } catch (err) {
+    console.error('Database Connection Failed:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDatabase;
