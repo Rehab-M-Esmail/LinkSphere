88ff52971da2774abeb27733d4aa858ef398a79e
BasmalaAmr1 <148448444+BasmalaAmr1@users.noreply.github.com>
2025-05-09 16:07:06 +0300

    Delete notificationService directory

--- notificationService/src/config/cache.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require('redis');
-const client = createClient({ url: process.env.REDIS_URL });
-client.connect()
-  .then(() => console.log('Redis connected - NotificationService'))
-  .catch(err => console.error('Redis error:', err));
-module.exports = client;
