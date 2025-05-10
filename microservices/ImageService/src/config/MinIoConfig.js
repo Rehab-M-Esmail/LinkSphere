3d3536b1547b34bed2a6f2426694d09876bbdd51
Rehab Mahmoud <rehabmahmoud521@gmail.com>
2025-05-10 07:48:55 +0300

    slight modification in minio configuration

--- microservices/ImageService/src/config/MinIoConfig.js
+++ microservices/ImageService/src/config/MinIoConfig.js
@@ -4,7 +4,7 @@ dotenv.config(); // An issue here exists cz of dotenv file not being found
 const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT || "localhost",
   port: process.env.MINIO_PORT || 9000,
-  useSSL: process.env.MINIO_USE_SSL || false,
+  useSSL: process.env.MINIO_USE_SSL === "false",
   accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
   secretKey: process.env.MINIO_SECRET_KEY || "minioadmin",
   region: process.env.MINIO_REGION || "me-central-1",
